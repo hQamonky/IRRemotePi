91bbf2f55b0dce2999fe10812010a72e029251da
qmk <wherlicq@gmail.com>
2020-05-30 22:49:56 +0200

    Trying to fix pip3.

--- fix_pip3_upgrade.sh
+++ fix_pip3_upgrade.sh
@@ -1,2 +1,2 @@
 if [ ! -e /usr/bin/pip ]; then ln -s pip3 /usr/bin/pip ; fi && \
-if [[ ! -e /usr/bin/python ]]; then ln -sf /usr/bin/python3 /usr/bin/python; fi && \
+if [[ ! -e /usr/bin/python ]]; then ln -sf /usr/bin/python3 /usr/bin/python; fi
