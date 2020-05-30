ed53de710a842de3d3ac0ecc48acc9a036f0f944
qmk <wherlicq@gmail.com>
2020-05-30 21:40:34 +0200

    Tested and updated device management methods. Created and tested command management methods. Added database/clear endpoint.

--- src/controller/ir.py
+++ src/controller/ir.py
@@ -1,16 +1,46 @@
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import board
 import pulseio
 import array
 import time
 
-# Set GPIO pins
-# TR = 12  # Transmitter GPIO pin
-# RR = board.D13  # Receiver GPIO pin
 
-# Initiate GPIO
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(TR, GPIO.OUT)
-# p = GPIO.PWM(TR, 38000)
-# ir_read = pulseio.PulseIn(RR, maxlen=100, idle_state=True)
-# ir_read.pause()
+class IR:
+    # Set GPIO pins
+    TR_pin = 12  # Transmitter GPIO pin
+    RR_pin = board.D13  # Receiver GPIO pin
+    IR_TR = None
+    IR_RR = None
+
+    def __init__(self):
+        # Initiate GPIO
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.TR_pin, GPIO.OUT)
+        # Initiate ir transmitter variable
+        self.IR_TR = GPIO.PWM(self.TR_pin, 38000)
+        # Initiate ir receiver variable
+        self.IR_RR = pulseio.PulseIn(self.RR_pin, maxlen=100, idle_state=True)
+        self.IR_RR.pause()
+
+    def start_recording(self):
+        self.IR_RR.resume()
+
+    def stop_recording(self):
+        self.IR_RR.pause()
+        signal = array.array('H', [self.IR_RR[x] for x in range(len(self.IR_RR))])
+        self.reset_rr()
+        return signal
+
+    def reset_rr(self):
+        self.IR_RR.clear()
+
+    def send(self, command):
+        on = False
+        for timer in command:
+            if on:
+                self.IR_TR.stop()
+                on = False
+            else:
+                self.IR_TR.start(50)
+                on = True
+            time.sleep(timer / 1000000)
