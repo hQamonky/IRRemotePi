f3a65fdb44f3876029809c389a0eb6771dbbd849
qmk <wherlicq@gmail.com>
2020-05-31 11:38:45 +0200

    Fixing array json problem.

--- src/controller/ir.py
+++ src/controller/ir.py
@@ -28,6 +28,7 @@ class IR:
     def stop_recording(self):
         self.IR_RR.pause()
         signal = array.array('H', [self.IR_RR[x] for x in range(len(self.IR_RR))])
+        signal = [self.IR_RR[x] for x in range(len(self.IR_RR))]
         self.reset_rr()
         return signal
 
