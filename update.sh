8f975ba4803a313412d47aa4dabe6a4588760569
qmk <wherlicq@gmail.com>
2020-06-16 22:46:31 +0200

    Hotfix: Added `chmod +x` on update files.

--- update.sh
+++ update.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# Update
 git -C ~/qmk/IRRemotePi pull
+# Set permissions
+chmod +x ~/qmk/IRRemotePi/run.sh
+chmod +x ~/qmk/IRRemotePi/update.sh
+chmod +x ~/qmk/IRRemotePi/update_force.sh
 # Restart service
 sudo systemctl restart qmk_irpi
-chmod +x ~/qmk/IRRemotePi/run.sh
--- update_force.sh
+++ update_force.sh
@@ -4,6 +4,9 @@
 git -C ~/qmk/IRRemotePi reset --hard
 # Update
 git -C ~/qmk/IRRemotePi pull
+# Set permissions
 chmod +x ~/qmk/IRRemotePi/run.sh
+chmod +x ~/qmk/IRRemotePi/update.sh
+chmod +x ~/qmk/IRRemotePi/update_force.sh
 # Restart service
 sudo systemctl restart qmk_irpi
